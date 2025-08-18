85bc110e170df560225294c07a8fa0301a3715f7
Jonah Riley Huggins <86029877+JonahRileyHuggins@users.noreply.github.com>
2025-08-18 16:02:35 -0400

    WIP: SingleCell Build command works!

--- Install.sh
+++ Install.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 
 # copy project files into local app directory
-mkdir ~/.local/share/SingleCell
-cp -r ./* ~/.local/share/SingleCell
-
-# persistent environmental variable
-echo "export SINGLECELL_PATH=~/'.local/share/SingleCell'" >> ~/.bashrc
-
-# Install pipx tool
-pipx ensurepath#!/bin/bash
-set -e  # exit on first error
-set -x  # print each command before executing
-
-# copy project files into local app directory
 mkdir -p ~/.local/share/SingleCell
 cp -r ./* ~/.local/share/SingleCell
 
@@ -21,7 +9,7 @@ echo 'export SINGLECELL_PATH="$HOME/.local/share/SingleCell"' >> ~/.bashrc
 
 # Install pipx tool
 pipx ensurepath
-pipx install ./python/dist/singlecell-0.0.1-py3-none-any.whl --verbose --force
+pipx install --python python3.12 ./python/dist/singlecell-0.0.1-py3-none-any.whl --verbose --force
 
 # reset and exit 
 echo -e "exiting...\n"
