857bf59e65ffd1a2c8a93b26d20543397a585579
JonahRileyHuggins <jonahrileyhuggins@gmail.com>
2026-05-12 16:01:31 -0400

    Corrected misplaced subprocess.run() command in singlecell build process

--- python/sdist/singlecell/ModelBuilding/singlecell_builder.py
+++ python/sdist/singlecell/ModelBuilding/singlecell_builder.py
@@ -42,8 +42,12 @@ def build_singlecell(
 
     # build commands, again mimicing cmake build process
     subprocess.run(
-        ['cmake', '--build', str(build_dir)], 
-        '--parallel',
+        [
+            'cmake', 
+            '--build', 
+            str(build_dir), 
+            '--parallel'
+        ],
         check=True
     )
 
